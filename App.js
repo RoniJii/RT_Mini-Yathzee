3f4421d78dba050387add68957ae24d06fdcaad7
RoniJii <81521266+RoniJii@users.noreply.github.com>
2022-09-27 15:54:57 +0300

    Ylimääräset pois

--- App.js
+++ App.js
@@ -1,5 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Gameboard from './components/Gameboard'
